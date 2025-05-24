a9826c9f10efd2647216fc86c8091c3af97728b3
legendaryxyn <legendaryxyn@gmail.com>
2025-05-24 13:10:46 +0800

    add configuration about docker

--- Program.cs
+++ Program.cs
@@ -2,27 +2,49 @@ using LeaderboardService.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure Kestrel to listen on all interfaces
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    serverOptions.ListenAnyIP(80);
+});
 
 // Add services to the container.
 builder.Services.AddSingleton<LeaderboardManager>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// Configure Swagger
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo 
+    { 
+        Title = "Leaderboard API", 
+        Version = "v1",
+        Description = "A simple leaderboard service API"
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// Always enable Swagger in Docker container
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Leaderboard API V1");
+    c.RoutePrefix = "swagger";
+});
 
-app.UseHttpsRedirection();
+// Comment out HTTPS redirection in Docker
+// app.UseHttpsRedirection();
+
 app.UseAuthorization();
 app.MapControllers();
+
+// Add a health check endpoint
+app.MapGet("/health", () => "Healthy");
 
 app.Run();
